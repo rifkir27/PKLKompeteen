1a69acba72f1176a873cd6a8e76ef71a886e031c
Rizky Alfin Pratama <alf290306@gmail.com>
2024-04-01 15:49:41 +0700

    Fix Sub Title Mentor dan Infrastructure

--- resources/views/admin/infrastructure/edit.blade.php
+++ resources/views/admin/infrastructure/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.backend.app', ['title' => 'Edit Course'])
+@extends('layouts.backend.app', ['title' => 'Edit Infrastructure'])
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
@@ -12,12 +12,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Edit Course</h1>
+                        <h1>Edit Infrastructure</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                            <li class="breadcrumb-item active">Edit Course</li>
+                            <li class="breadcrumb-item active">Edit Infrastructure</li>
                         </ol>
                     </div>
                 </div>
